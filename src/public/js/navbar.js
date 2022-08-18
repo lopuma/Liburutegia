64f21be5e97ec8da5a88caa847c4ed63e9f62c3c
lopuma <lopumacp@hotmail.com>
2022-08-18 03:30:38 +0200

    table/mobil

--- src/public/js/navbar.js
+++ src/public/js/navbar.js
@@ -1,3 +1,24 @@
+const drop = document.querySelector('.dropdown');
+
+/**
+ * Al pasar el raton me habre el menu contextual
+ */
+drop.addEventListener('mouseenter', () => {
+    $this = '.dropdown';
+    $($this).addClass('show');
+    $($this).find('.dropdown-toggle').attr("aria-expanded", "true");
+    $($this).find('.dropdown-menu').addClass('show');
+})
+
+/**
+ * Al pasar el raton me cierra el menu contextual
+ */
+drop.addEventListener('mouseleave', () => {
+    $this = '.dropdown';
+    $($this).removeClass('show');
+    $($this).find('.dropdown-toggle').attr("aria-expanded", "false");
+    $($this).find('.dropdown-menu').removeClass('show');
+})
 
 const btnToggle = document.querySelector('#btnToggle');
 const navBar = document.querySelector('#navBar');
