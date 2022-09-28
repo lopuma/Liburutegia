cfc9a34b001d0b207bbadba8a1410173c7be59e8
Jose-Alvaro-Cedeno-Panchana <Jose.Alvaro.Cedeno.Panchana@ocean.ibm.com>
2022-09-28 17:59:02 +0200

    V3

--- src/routes/db/userDefault.js
+++ src/routes/db/userDefault.js
@@ -1,26 +1,9 @@
-
-const express = require('express');
-const router = express.Router();
-
-// Invocamos a la conexion de la DB
-// Invocamos a bscryptjs
 const connection = require("../../../database/db");
 const bscryptjs = require("bcryptjs");
-
+const { TEST_USER_EMAIL, HASH_KEYWORD, DEFAULT_USER_DATA, sql, sqlInsert } = require("../../../env/dataUserDefault")
 async function defaultUser() {
-    var sql = "SELECT IF( EXISTS(SELECT * FROM users WHERE email='admin@mail.com'), 1, 0) AS response";
-    var sqlInsert = "INSERT INTO users SET ?";
-    let passwordHash = await bscryptjs.hash('admin', 8);
-
-    userDefault = {
-        email:'admin@mail.com',
-        firstname:'Admin',
-        lastname:'Admin',
-        rol:'admin',
-        pass: passwordHash,
-        _ss:0
-    }
-
+    const passwordHash = await bscryptjs.hash(`${HASH_KEYWORD}`, 8);
+    const userDefault = {...DEFAULT_USER_DATA, pass: passwordHash};
     connection.query(sql, async (error, results) => {
         if (error) throw error;
         exists = await results[0].response;
