5e9148e9df08bf3ff2f3e5b0113ea361fad61340
lopuma <lopumacp@hotmail.com>
2023-02-04 04:44:31 +0100

    desploy

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const { database } = require('../database/keys.js');
-
+console.log("Database ", database)
 const connection = mysql.createConnection(database);
 connection.connect(err => {
     if (err) {
@@ -8,6 +8,7 @@ connection.connect(err => {
             console.console.console.error();("Database connection was refused");
             return;
         }
+        console.error(err);
     } else {
         console.info(`The DataBase is connected on the PORT: ${database.port}`);
     }
--- database/keys.js
+++ database/keys.js
@@ -10,8 +10,9 @@ module.exports = {
         host: config.MYSQL_HOST,
         port: config.MYSQL_PORT,
         user: config.MYSQL_USER,
-        password: config.MYSQL_ROOT_PASSWORD,
-        database: config.MYSQL_DATABASE
+        password: config.MYSQL_PASSWORD,
+        database: config.MYSQL_DATABASE,
+        protocol: 'TCP'
     }
 };
 
