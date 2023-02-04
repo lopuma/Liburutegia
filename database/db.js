a7e4d06031f73817ff82074d200a11f5981bc9ac
Jose Alvaro Cedeño Panchana <86171869+lopuma@users.noreply.github.com>
2023-02-04 12:47:20 +0100

    Database

--- database/db.js
+++ database/db.js
@@ -2,7 +2,10 @@ const mysql = require("mysql");
 const { database } = require('../database/keys.js');
 console.log("Database ", database)
 const connection = mysql.createConnection('mysql://root:wNv0RgxOXeBzpyeFMnY9@containers-us-west-197.railway.app:6284/railway');
-connection.connect(err => {
+connection.on('error', function(err) {
+  console.log("[mysql error]",err);
+});
+/*connection.connect(err => {
     if (err) {
         if (err.code === "ER_NOT_SUPPORTED_AUTH_MODE") {
             console.console.console.error();("Database connection was refused");
@@ -12,6 +15,6 @@ connection.connect(err => {
     } else {
         console.info(`The DataBase is connected on the PORT: ${database.port}`);
     }
-});
+});*/
 
 module.exports = connection;
